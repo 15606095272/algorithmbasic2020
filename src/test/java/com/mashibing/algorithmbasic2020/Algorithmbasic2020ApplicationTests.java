eee0de422ae52c03c118ab0f1ba98eb84663d368
15606095272 <1227962369@qq.com>
2022-02-26 22:56:08 +0800

    init

---
+++ src/test/java/com/mashibing/algorithmbasic2020/Algorithmbasic2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mashibing.algorithmbasic2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Algorithmbasic2020ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
