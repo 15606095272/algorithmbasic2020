e106c02128c1417e2465ca19f1593e5d78a89cd1
15606095272 <1227962369@qq.com>
2022-02-26 23:51:59 +0800

    select sort and util

---
+++ src/main/java/com/mashibing/algorithmbasic2020/class_01/Code00_RandomArrayUtil.java
@@ -0,0 +1,87 @@
+package com.mashibing.algorithmbasic2020.class_01;
+
+import java.util.Arrays;
+
+public class Code00_RandomArrayUtil {
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] newArr = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            newArr[i] = arr[i];
+        }
+        return newArr;
+    }
+
+    public static void sort(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    public static int[] randomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) (Math.random() * (maxSize + 1))];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * (maxValue + 1)) - (int) (Math.random() * maxValue);
+        }
+
+        return arr;
+    }
+
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int maxSize = 10;
+        int maxValue = 100;
+        int times = 500000;
+        boolean success = true;
+        for (int i = 0; i < times; i++) {
+            int[] arr = randomArray(maxSize, maxValue);
+            int[] arr1 = copyArray(arr);
+            int[] arr2 = copyArray(arr);
+            sort(arr1);
+            Code01_SelectSort.selectSort(arr2);
+            if (!isEqual(arr1, arr2)) {
+                success = false;
+                printArray(arr1);
+                printArray(arr2);
+                break;
+            }
+        }
+
+        System.out.println(success ? "Nice!" : "Fucking fucked!");
+    }
+}
