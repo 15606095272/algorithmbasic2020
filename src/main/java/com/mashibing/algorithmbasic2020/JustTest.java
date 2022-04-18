9891ba047501076b088d88537d363870f25951a5
linshijin <linshijin@leedarson.com>
2022-04-18 20:28:26 +0800

    判断链表是否相交

--- src/main/java/com/mashibing/algorithmbasic2020/JustTest.java
+++ src/main/java/com/mashibing/algorithmbasic2020/JustTest.java
@@ -1,9 +1,25 @@
 package com.mashibing.algorithmbasic2020;
 
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
 public class JustTest {
 
     public static void main(String[] args) {
-//        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor();
-//        threadPoolExecutor.execute();
+        LocalDate localDate = LocalDate.now();
+        LocalDate yesterday = localDate.plusDays(-1);
+        LocalDate yesterday2 = localDate.plusDays(-1);
+        LocalDate tomorrow = localDate.plusDays(1);
+        System.out.println(yesterday);
+        System.out.println(localDate);
+        System.out.println(tomorrow);
+        System.out.println(yesterday.isEqual(yesterday2));
+        Map<LocalDate, Integer> map = new HashMap<>();
+        map.put(yesterday, 1);
+        map.put(localDate, 2);
+        map.put(tomorrow, 3);
+        System.out.println(map);
+        System.out.println(map.get(yesterday2));
     }
 }
