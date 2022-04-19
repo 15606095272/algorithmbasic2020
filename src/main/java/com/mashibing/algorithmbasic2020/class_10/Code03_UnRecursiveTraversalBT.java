ffeb9ddeab169eda59abcd0159ab651942487403
linshijin <linshijin@leedarson.com>
2022-04-19 11:29:16 +0800

    二叉树递归、非递归遍历实现、前序、中序、后序

---
+++ src/main/java/com/mashibing/algorithmbasic2020/class_10/Code03_UnRecursiveTraversalBT.java
@@ -0,0 +1,120 @@
+package com.mashibing.algorithmbasic2020.class_10;
+
+import java.util.Stack;
+
+/**
+ * @author linshijin
+ * @date 2022/4/19 11:09
+ */
+public class Code03_UnRecursiveTraversalBT {
+
+    public static class Node {
+        private int value;
+        private Node left;
+        private Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static void pre(Node head) {
+        System.out.println("pre-order: ");
+        if (head != null) {
+            Stack<Node> stack = new Stack<>();
+            stack.push(head);
+            while (!stack.isEmpty()) {
+                head = stack.pop();
+                System.out.println(head.value + "");
+                if (head.right != null) {
+                    stack.push(head.right);
+                }
+                if (head.left != null) {
+                    stack.push(head.left);
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public static void in(Node cur) {
+        System.out.println("in-order: ");
+        if (cur != null) {
+            Stack<Node> stack = new Stack<>();
+            while (cur != null || !stack.isEmpty()) {
+                if (cur != null) {
+                    stack.push(cur);
+                    cur = cur.left;
+                } else {
+                    cur = stack.pop();
+                    System.out.println(cur.value + "");
+                    cur = cur.right;
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public static void pos1(Node head) {
+        System.out.println("pos-order: ");
+        if (head != null) {
+            Stack<Node> stack1 = new Stack<>();
+            Stack<Node> stack2 = new Stack<>();
+            stack1.push(head);
+            while (!stack1.isEmpty()) {
+                head = stack1.pop();
+                stack2.push(head);
+                if (head.left != null) {
+                    stack1.push(head.left);
+                }
+                if (head.right != null) {
+                    stack1.push(head.right);
+                }
+            }
+            while (!stack2.isEmpty()) {
+                System.out.println(stack2.pop().value + "");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void pos2(Node h) {
+        System.out.print("pos-order: ");
+        if (h != null) {
+            Stack<Node> stack = new Stack<Node>();
+            stack.push(h);
+            Node c = null;
+            while (!stack.isEmpty()) {
+                c = stack.peek();
+                if (c.left != null && h != c.left && h != c.right) {
+                    stack.push(c.left);
+                } else if (c.right != null && h != c.right) {
+                    stack.push(c.right);
+                } else {
+                    System.out.print(stack.pop().value + " ");
+                    h = c;
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.left = new Node(2);
+        head.right = new Node(3);
+        head.left.left = new Node(4);
+        head.left.right = new Node(5);
+        head.right.left = new Node(6);
+        head.right.right = new Node(7);
+
+        pre(head);
+        System.out.println("========");
+        in(head);
+        System.out.println("========");
+        pos1(head);
+        System.out.println("========");
+        pos2(head);
+        System.out.println("========");
+    }
+}
